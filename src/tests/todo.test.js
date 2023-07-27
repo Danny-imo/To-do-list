511394a6e4a00a4a91ea71f771f8775dc3b43d81
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-07-27 14:45:23 +0500

    add test code for deleteTask

--- src/tests/todo.test.js
+++ src/tests/todo.test.js
@@ -1,4 +1,4 @@
-import { addTask } from '../modules/todo.js';
+import { addTask, deleteTask } from '../modules/todo.js';
 
 document.body.innerHTML = `
   <ul id="task-list">
@@ -43,6 +43,26 @@ describe('Todo Functions', () => {
 
       // Assert that the DOM contains the new task <li> element
       expect(taskList.children).toHaveLength(1);
+    });
+  });
+
+  describe('deleteTask', () => {
+    test('it should remove a task from the tasks array and the DOM', () => {
+      // Mock initial tasks in localStorage
+      const initialTasks = [
+        { description: 'Task 1', completed: false, index: 1 },
+        { description: 'Task 2', completed: false, index: 2 },
+      ];
+      localStorage.setItem('tasks', JSON.stringify(initialTasks));
+
+      // Call the deleteTask function to delete the first task (index 0)
+      deleteTask(0);
+
+      // Get the updated tasks from localStorage
+      const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+      // Assert that the tasks array is updated and the first task is removed
+      expect(tasks).toHaveLength(0);
     });
   });
 });
